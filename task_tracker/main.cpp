4074844204bb954a7127c51f23cbdb66fedb290f
孟安凝 <anna.mir495@gmail.com>
2026-05-11 19:56:08 +0300

    Update main.cpp

--- task_tracker/main.cpp
+++ task_tracker/main.cpp
@@ -4,23 +4,156 @@
 
 using namespace std;
 
+//================== Additional class TaskType ===================
+class TaskType
+{
+private:
+    string typeName;
+public:
+    // Конструктор
+    TaskType(string name)
+    {
+        typeName = name;
+    }
+
+    // Геттеры
+    string getTypeName() const { return typeName; }
+
+    //question
+    // Вывод информации о позиции
+    void printTaskType() const
+    {
+        cout << typeName;
+    }
+
+    // Статические методы для создания предопределённых типов
+    static TaskType createBugTask()
+    {
+        return TaskType("Баг");
+    }
+
+    static TaskType createFeatureTask()
+    {
+        return TaskType("Фича");
+    }
+
+    static TaskType createDocumentationTask()
+    {
+        return TaskType("Документация");
+    }
+
+    // Метод для получения позиции по строковому вводу
+    static TaskType getTypeByString(const string& input)
+    {
+        string lowerInput = input;
+        for (int i = 0; i < lowerInput.length(); i++)
+        {
+            lowerInput[i] = tolower(lowerInput[i]);
+        }
+
+        if (lowerInput == "Баг" || lowerInput == "баг" ||
+            lowerInput == "Bug" || lowerInput == "bug")
+        {
+            return createBugTask();
+        }
+        else if (lowerInput == "Фича" || lowerInput == "фича" ||
+            lowerInput == "Feature" || lowerInput == "feature")
+        {
+            return createFeatureTask();
+        }
+        else if (lowerInput == "Документация" || lowerInput == "документация" ||
+            lowerInput == "Documentation" || lowerInput == "documentation")
+        {
+            return createDocumentationTask();
+        }
+        else
+        {
+            // По умолчанию возвращаем баг
+            cout << "Тип задачи не распознан. Будет установлено значение Баг по умолчанию." << endl;
+            return createBugTask();
+        }
+    }
+};
+
 //================== Base abstract class Task ===================
 class Task {
-protected:
-    int taskId;
-    string taskTitle;
-    string taskDescription;
-    string taskPriority;
-    string taskStatus;
-    string taskAssignee;
+private:
+    static int nextId; // счётчик id
+    int taskId; // уникальный номер id
+    string taskTitle; // краткое название задачи
+    string taskDescription; // подробное описание задачи
+    string taskPriority; // приоритет задачи («Низкий», «Средний», «Высокий», «Критический»)
+    string taskStatus; // статус задачи («Открыта», «В работе», «На проверке», «Закрыта»)
+    string taskAssignee; // ФИО или имя исполнителя
+    TaskType type; //композиция: задвча имеет тип
 public:
-    string getTaskTipe();
-    string getDetailedInfo();
+    // конструктор по умолчанию
+    Task() : type(TaskType::createBugTask()) {
+        taskId = nextId++; // автоматическое увеличение id
+        taskTitle = "New Task";
+        taskDescription = "Task to be done";
+        taskPriority = "High";
+        taskStatus = "In progress";
+        taskAssignee = "Nobody";
+    }
+
+    // контруктор
+    Task(int id, string title, string description,
+        string priority, string status, string assignee, const TaskType& t) : type(t)
+    {
+        // question
+        taskId = nextId++;
+        // question
+        taskDescription = description;
+        taskPriority = priority;
+        taskStatus = status;
+        taskAssignee = assignee;
+    }
+
+    ~Task() { 
+        // тип уничтожается автоматически вместе с задачей
+    }
+
+    // геттеры
+    string getTaskTitle() { return taskTitle; }
+    string getTaskDescription() { return taskDescription; }
+    string getTaskPriority() { return taskPriority; }
+    string getTaskStatus() { return taskStatus; }
+    string getTaskAssignee() { return taskAssignee; }
+    TaskType getTaskType() const { return type; }
+    
+    // сеттеры
+    void setTaskTitle(string title) { taskTitle = title; }
+    void setTaskDescription(string description) { taskDescription = description; }
+    void setTaskPriority(string priority) { taskPriority = priority; }
+    void setTaskStatus(string status) { taskStatus = status; }
+    void setTaskAssignee(string assignee) { taskAssignee = assignee; }
+
+    void printDetailedInfo() {
+        cout << "-----------------------------------------" << endl;
+        cout << "|                Task Info              |" << endl;
+        cout << "-----------------------------------------" << endl;
+        cout << " Title        : " << taskTitle << endl;
+        cout << " Description  : " << taskDescription << endl;
+        cout << " Priority     : " << taskPriority << endl;
+        cout << " Status       : " << taskStatus << endl;
+        cout << " Assignee     : " << taskAssignee << endl;
+        cout << " Type         : ";
+        type.printTaskType();
+        cout << endl;
+        cout << "-----------------------------------------------" << endl;
+    };
 
     void printCard();
     void advanceStatus();
     void reopen();
+
+    //tester
+    int getId() const { return taskId;  }
 };
+
+// Инициализация статической переменной
+int Task::nextId = 1;
 
 //================== Derived class BugTask ===================
 class BugTask : Task {
@@ -52,6 +185,8 @@ public:
     string getDetailedInfo();
 };
 
+
+
 /*class KanbanBoard {
     vector<Column> columns; // Todo, InProgress, Done
     map<string, Task> tasks;
@@ -62,5 +197,12 @@ public:
 };*/
 
 int main() {
-    return 0;
+    Task myTask1;
+    cout << "id: " << myTask1.getId() << endl;
+    Task myTask2;
+    cout << "id: " << myTask2.getId() << endl;
+    Task myTask3;
+    cout << "id: " << myTask3.getId() << endl;
+
+    myTask2.printDetailedInfo();
 };
